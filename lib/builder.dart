5ec9ecc47a72e220d991e7cb7e2fbba6fc486efc
Lucas Garcia de Araújo Martins <lucas.araujo@ifgoiano.edu.br>
2019-12-17 12:45:56 -0300

    Entity generation is with extension .entity.dart

--- lib/builder.dart
+++ lib/builder.dart
@@ -2,4 +2,4 @@ import 'package:build/build.dart';
 import 'package:flutter_persistence_firestore/src/model_generator.dart';
 import 'package:source_gen/source_gen.dart';
 
-Builder entityBuilder(BuilderOptions options) => LibraryBuilder(EntityGenerator(), generatedExtension: '.model.dart');
+Builder entityBuilder(BuilderOptions options) => LibraryBuilder(EntityGenerator(), generatedExtension: '.entity.dart');
