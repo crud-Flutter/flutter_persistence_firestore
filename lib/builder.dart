055b897a9b39dd4d1e5ccdb8d7f6c3d9409dd03b
Lucas Garcia de Araujo Martins <lukasgarcya@hotmail.com>
2019-12-17 08:51:04 -0300

    Implementation for entity builder

---
+++ lib/builder.dart
@@ -0,0 +1,5 @@
+import 'package:build/build.dart';
+import 'package:flutter_persistence_firestore/src/model_generator.dart';
+import 'package:source_gen/source_gen.dart';
+
+Builder entityBuilder(BuilderOptions options) => LibraryBuilder(EntityGenerator(), generatedExtension: '.model.dart');
