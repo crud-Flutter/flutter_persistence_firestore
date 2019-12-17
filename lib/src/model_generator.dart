ee46274b2d813d2606fa43f40793e84a594bb885
Lucas Garcia de Araújo Martins <lucas.araujo@ifgoiano.edu.br>
2019-12-17 12:47:23 -0300

    Generation for Class Entity

---
+++ lib/src/model_generator.dart
@@ -0,0 +1,19 @@
+import 'package:analyzer/dart/element/element.dart';
+import 'package:build/src/builder/build_step.dart';
+import 'package:flutter_persistence_api/flutter_persistence_api.dart';
+import 'package:flutter_persistence_firestore/src/class_source.dart';
+import 'package:source_gen/source_gen.dart';
+
+class EntityGenerator extends GeneratorForAnnotation<Entity> {
+  @override
+  generateForAnnotatedElement(
+      Element element, ConstantReader annotation, BuildStep buildStep) {
+    GenerateModelClass generateClass = new GenerateModelClass(element.name);    
+    var fieldAnnotation = TypeChecker.fromRuntime(Field);
+    for (var field in (element as ClassElement).fields) {
+      generateClass.addField(field.type.name, field.name,
+          persistField: fieldAnnotation.hasAnnotationOfExact(field));
+    }
+    return generateClass.build();
+  }
+}
