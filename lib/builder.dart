caac70696b68d83b177175b5b038d0b6ec77dec4
Lucas Garcia de Araújo Martins <lucas.araujo@ifgoiano.edu.br>
2019-12-17 14:46:02 -0300

    Add builder for repository

--- lib/builder.dart
+++ lib/builder.dart
@@ -1,5 +1,7 @@
 import 'package:build/build.dart';
 import 'package:flutter_persistence_firestore/src/model_generator.dart';
+import 'package:flutter_persistence_firestore/src/repository_generator.dart';
 import 'package:source_gen/source_gen.dart';
 
 Builder entityBuilder(BuilderOptions options) => LibraryBuilder(EntityGenerator(), generatedExtension: '.entity.dart');
+Builder repositoryBuilder(BuilderOptions options) => LibraryBuilder(RepositoryGenerator(), generatedExtension: '.repository.dart');
