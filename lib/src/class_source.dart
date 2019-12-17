99f86020d3b511e1f3c286abf2271bd2403f2f7e
Lucas Garcia de Araújo Martins <lucas.araujo@ifgoiano.edu.br>
2019-12-17 14:47:20 -0300

    Class generated for Entity and Repository.

--- lib/src/class_source.dart
+++ lib/src/class_source.dart
@@ -1,32 +1,45 @@
-class GenerateClass {
+abstract class GenerateClass {
   String name;
+  String classPrefix;
+  String classSuffix;
   String parentClass;
   StringBuffer generateClass = new StringBuffer();
-  GenerateClass(this.name, {String import, this.parentClass}) {    
-    if (import != null) generateClass.writeln(import);
+  GenerateClass(this.classPrefix, {this.classSuffix, this.parentClass}) {
+    this.name = this.classPrefix;
+    if (this.classSuffix != null) {
+      this.name += this.classSuffix;
+    }
+    addImports();
     _setClass();
   }
 
   _setClass() {
-    // generateClass.writeln('class $name {');
     String declaredClass = 'class $name';
     if (this.parentClass != null) {
-      declaredClass += 'extends $parentClass';
+      declaredClass += ' extends $parentClass';
     }
     declaredClass += ' {';
     generateClass.writeln(declaredClass);
   }
+
+  constructorEmpty() {
+    generateClass.writeln('$name();');
+  }
+
+  String build() {
+    generateClass.write('}');
+    return generateClass.toString();
+  }
+
+  addImports();
 }
 
 class GenerateModelClass extends GenerateClass {
   Map<String, String> fields = new Map();
 
-  GenerateModelClass(String name)
-      : super(name+'Entity',
-            import:
-                'import \'package:cloud_firestore/cloud_firestore.dart\';') {
+  GenerateModelClass(String name) : super(name, classSuffix: 'Entity') {
     generateClass.writeln('String _documentId;');
-    generateClass.writeln('$name'+'Entity();');
+    this.constructorEmpty();
     generateClass.writeln('String documentId() => this._documentId;');
   }
 
@@ -58,7 +71,7 @@ class GenerateModelClass extends GenerateClass {
   }
 
   _toMap() {
-    if (fields.length>0) {
+    if (fields.length > 0) {
       generateClass.writeln('toMap() {');
       generateClass.writeln('var map = new Map<String, dynamic>();');
       fields.forEach((name, type) {
@@ -72,7 +85,64 @@ class GenerateModelClass extends GenerateClass {
   String build() {
     this._fromMap();
     this._toMap();
-    generateClass.writeln('}');
-    return generateClass.toString();
+    return super.build();
+  }
+
+  @override
+  addImports() {
+    generateClass
+        .writeln('import \'package:cloud_firestore/cloud_firestore.dart\';');
+  }
+}
+
+class GenerateRepositoryClass extends GenerateClass {
+  String entityInstance;
+  String entityClassInstance;
+  String entityClass;
+  GenerateRepositoryClass(String name)
+      : super(name, classSuffix: 'Repository') {
+    // GenerateRepositoryClass(String name): super(name+'Repository', parentClass: 'Disposable') {
+    this.entityInstance = name.toLowerCase() + 'Entity';
+    this.entityClass = this.classPrefix + 'Entity';
+    this.entityClassInstance = '$entityClass $entityInstance';
+    _reference();
+    _add();
+    _update();
+    _delete();
+    _list();
+  }
+
+  _reference() {
+    String collection = this.classPrefix.toLowerCase();
+    generateClass.writeln(
+        'CollectionReference _collection = Firestore.instance.collection(\'$collection\');');
+  }
+
+  _add() {
+    generateClass.writeln(
+        'void add($entityClassInstance) => _collection.add($entityInstance.toMap());');
+  }
+
+  _update() {
+    generateClass.writeln(
+        'void update(String documentId, $entityClassInstance) => _collection.document(documentId).updateData($entityInstance.toMap());');
+  }
+
+  _delete() {
+    generateClass.writeln(
+        'void delete(String documentId) => _collection.document(documentId).delete();');
+  }
+
+  _list() {
+    generateClass.writeln(
+        'Stream<List<$entityClass>> get $entityInstance => _collection.snapshots().map((snapshot) => snapshot.documents.map<$entityClass>((document) => $entityClass.fromMap(document)).toList());');
+  }
+
+  @override
+  addImports() {
+    String fileEntity = this.classPrefix.toLowerCase() + '.entity.dart';
+    generateClass.writeln('import \'$fileEntity\';');    
+    generateClass
+        .writeln('import \'package:cloud_firestore/cloud_firestore.dart\';');    
   }
 }
