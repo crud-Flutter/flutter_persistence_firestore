d3156bf9c1934abce67a8ac3a4031b5748ea1502
Lucas Garcia de Araújo Martins <lucas.araujo@ifgoiano.edu.br>
2019-12-18 17:47:57 -0300

    import file direct in same project

--- lib/builder.dart
+++ lib/builder.dart
@@ -1,6 +1,6 @@
 import 'package:build/build.dart';
-import 'package:flutter_persistence_firestore/src/model_generator.dart';
-import 'package:flutter_persistence_firestore/src/repository_generator.dart';
+import 'src/entity_generator.dart';
+import 'src/repository_generator.dart';
 import 'package:source_gen/source_gen.dart';
 
 Builder entityBuilder(BuilderOptions options) => LibraryBuilder(EntityGenerator(), generatedExtension: '.entity.dart');
