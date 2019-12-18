49e56c9c751069aeb7148861c194f700029fa9e1
Lucas Garcia de Araújo Martins <lucas.araujo@ifgoiano.edu.br>
2019-12-18 13:49:42 -0300

    class for generation in this file

--- lib/src/repository_generator.dart
+++ lib/src/repository_generator.dart
@@ -1,7 +1,7 @@
 import 'package:analyzer/dart/element/element.dart';
 import 'package:build/src/builder/build_step.dart';
 import 'package:flutter_persistence_api/flutter_persistence_api.dart';
-import 'package:flutter_persistence_firestore/src/class_source.dart';
+import 'package:crud_generator/crud_generator.dart';
 import 'package:source_gen/source_gen.dart';
 
 class RepositoryGenerator extends GeneratorForAnnotation<Entity> {
@@ -10,4 +10,51 @@ class RepositoryGenerator extends GeneratorForAnnotation<Entity> {
     return GenerateRepositoryClass(element.name).build();
   }
   
+}
+
+class GenerateRepositoryClass extends GenerateEntityClassAbstract {
+  
+  GenerateRepositoryClass(String name)
+      : super(name, classSuffix: 'Repository') {
+    // GenerateRepositoryClass(String name): super(name+'Repository', parentClass: 'Disposable') {
+    
+    _reference();
+    _add();
+    _update();
+    _delete();
+    _list();
+  }
+
+  _reference() {
+    String collection = this.classPrefix.toLowerCase();
+    generateClass.writeln(
+        'CollectionReference _collection = Firestore.instance.collection(\'$collection\');');
+  }
+
+  _add() {
+    generateClass.writeln(
+        'void add($entityClassInstance) => _collection.add($entityInstance.toMap());');
+  }
+
+  _update() {
+    generateClass.writeln(
+        'void update(String documentId, $entityClassInstance) => _collection.document(documentId).updateData($entityInstance.toMap());');
+  }
+
+  _delete() {
+    generateClass.writeln(
+        'void delete(String documentId) => _collection.document(documentId).delete();');
+  }
+
+  _list() {
+    generateClass.writeln(
+        'Stream<List<$entityClass>> get $entityInstance => _collection.snapshots().map((snapshot) => snapshot.documents.map<$entityClass>((document) => $entityClass.fromMap(document)).toList());');
+  }
+
+  @override
+  addImports() {
+    importEntity();
+    generateClass
+        .writeln('import \'package:cloud_firestore/cloud_firestore.dart\';');
+  }
 }
