66b3f021744e76ae74c15c85a7d18f747db792f9
Lucas Garcia de Araújo Martins <lucas.araujo@ifgoiano.edu.br>
2019-12-18 13:49:23 -0300

    class for generation in this file

--- lib/src/model_generator.dart
+++ lib/src/model_generator.dart
@@ -1,7 +1,7 @@
 import 'package:analyzer/dart/element/element.dart';
 import 'package:build/src/builder/build_step.dart';
 import 'package:flutter_persistence_api/flutter_persistence_api.dart';
-import 'package:flutter_persistence_firestore/src/class_source.dart';
+import 'package:crud_generator/crud_generator.dart';
 import 'package:source_gen/source_gen.dart';
 
 class EntityGenerator extends GeneratorForAnnotation<Entity> {
@@ -15,5 +15,55 @@ class EntityGenerator extends GeneratorForAnnotation<Entity> {
           persistField: fieldAnnotation.hasAnnotationOfExact(field));
     }
     return generateClass.build();
+  }
+}
+
+class GenerateEntityClass extends GenerateClass {
+  GenerateEntityClass(String name) : super(name, classSuffix: 'Entity') {
+    generateClass.writeln('String _documentId;');
+    this.constructorEmpty();
+    generateClass.writeln('String documentId() => this._documentId;');
+  }
+
+  _fromMap() {
+    if (fields.length > 0) {
+      generateClass.writeln('$name.fromMap(DocumentSnapshot document) {');
+      generateClass.writeln('_documentId = document.documentID;');
+      fields.forEach((name, type) {
+        String value = 'document.data[\'$name\']';
+        if (type == 'DateTime') {
+          generateClass
+              .writeln('Timestamp timestamp = document.data[\'$name\'];');
+          value =
+              'DateTime.fromMillisecondsSinceEpoch(timestamp.millisecondsSinceEpoch)';
+        }
+        generateClass.writeln('this.$name = $value;');
+      });
+      generateClass.writeln('}');
+    }
+  }
+
+  _toMap() {
+    if (fields.length > 0) {
+      generateClass.writeln('toMap() {');
+      generateClass.writeln('var map = new Map<String, dynamic>();');
+      fields.forEach((name, type) {
+        generateClass.writeln('map[\'$name\'] = this.$name;');
+      });
+      generateClass.writeln('return map;');
+      generateClass.writeln('}');
+    }
+  }
+
+  String build() {
+    this._fromMap();
+    this._toMap();
+    return super.build();
+  }
+
+  @override
+  addImports() {
+    generateClass
+        .writeln('import \'package:cloud_firestore/cloud_firestore.dart\';');
   }
 }
