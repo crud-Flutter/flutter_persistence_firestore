0963354c8e6f3c37fd02b2252f94c0b95c59b663
Lucas Garcia de Araujo Martins <lukasgarcya@hotmail.com>
2019-12-22 13:59:30 -0300

    RepositoryGenerate used code_builder for generation of the code

--- lib/src/repository_generator.dart
+++ lib/src/repository_generator.dart
@@ -1,57 +1,84 @@
 import 'package:analyzer/dart/element/element.dart';
 import 'package:build/src/builder/build_step.dart';
+import 'package:code_builder/code_builder.dart';
 import 'package:flutter_persistence_api/flutter_persistence_api.dart';
 import 'package:crud_generator/crud_generator.dart';
 import 'package:source_gen/source_gen.dart';
 
-// class RepositoryGenerator extends GeneratorForAnnotation<Entity> {
-//   @override
-//   generateForAnnotatedElement(
-//       Element element, ConstantReader annotation, BuildStep buildStep) {
-//     return GenerateRepositoryClass(element.name).build();
-//   }
-// }
+class RepositoryGenerator extends GenerateEntityClassForAnnotation<Entity> {
+  @override
+  generateForAnnotatedElement(
+      Element element, ConstantReader annotation, BuildStep buildStep) {
+    this.element = element;
+    name = '${element.name}Repository';
+    _referenceField();
+    _methodAdd();
+    _methodUpdate();
+    _methodDelete();
+    _methodList();
 
-// class GenerateRepositoryClass extends GenerateEntityClassAbstract {
-  
-//   GenerateRepositoryClass(String name)
-//       : super(name, classSuffix: 'Repository') {
-//     _reference();
-//     _add();
-//     _update();
-//     _delete();
-//     _list();
-//   }
+    return "import 'package:cloud_firestore/cloud_firestore.dart';\n"
+            "import '${element.name.toLowerCase()}.entity.dart';" +
+        build();
+    // return GenerateRepositoryClass(element.name).build();
+  }
 
-//   _reference() {
-//     generateClass.writeln(
-//         'CollectionReference _collection = Firestore.instance.collection(\'$nameLowerCase\');');
-//   }
+  _referenceField() {
+    declareField(
+        refer('CollectionReference',
+            'package:cloud_firestore/cloud_firestore.dart'),
+        '_collection',
+        assignment: Code(
+            "Firestore.instance.collection('${element.name.toLowerCase()}')"));
+  }
 
-//   _add() {
-//     generateClass.writeln(
-//         'void add($entityClassInstance) => _collection.add($entityInstance.toMap());');
-//   }
+  _methodAdd() {
+    declareMethod('add',
+        returns: refer('Future<String>'),
+        requiredParameters: [
+          Parameter((b) => b
+            ..name = entityInstance
+            ..type = refer(entityClass))
+        ],
+        lambda: true,
+        body:
+            Code('(await _collection.add($entityInstance.toMap())).documentID'),
+        modifier: MethodModifier.async);
+  }
 
-//   _update() {
-//     generateClass.writeln(
-//         'void update(String documentId, $entityClassInstance) => _collection.document(documentId).updateData($entityInstance.toMap());');
-//   }
+  _methodUpdate() {
+    declareMethod('update',
+        returns: refer('Future<void>'),
+        requiredParameters: [
+          Parameter((b) => b
+            ..name = 'documentId'
+            ..type = refer('String')),
+          Parameter((b) => b
+            ..name = entityInstance
+            ..type = refer(entityClass))
+        ],
+        lambda: true,
+        body: Code(
+            '_collection.document(documentId).updateData($entityInstance.toMap())'));
+  }
 
-//   _delete() {
-//     generateClass.writeln(
-//         'void delete(String documentId) => _collection.document(documentId).delete();');
-//   }
+  _methodDelete() {
+    declareMethod('delete',
+        returns: refer('Future<void>'),
+        requiredParameters: [
+          Parameter((b) => b
+            ..name = 'documentId'
+            ..type = refer('String'))
+        ],
+        lambda: true,
+        body: Code(' _collection.document(documentId).delete()'));
+  }
 
-//   _list() {
-//     generateClass.writeln('Stream<List<$entityClass>> get $nameLowerCase'
-//         's => _collection.snapshots().map((snapshot) => snapshot.documents.map<$entityClass>((document) => $entityClass.fromMap(document)).toList());');
-//   }
-
-//   @override
-//   addImports() {
-//     importEntity();
-//     generateClass
-//         .writeln('import \'package:cloud_firestore/cloud_firestore.dart\';');
-//   }
-// }
+  _methodList() {
+    declareMethod('list',
+        returns: refer('Stream<List<$entityClass>>'),
+        lambda: true,
+        body: Code(
+            '_collection.snapshots().map((snapshot) => snapshot.documents.map<$entityClass>((document) => $entityClass.fromMap(document)).toList())'));
+  }
+}
