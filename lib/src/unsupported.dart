d83f975df55faf5840578ef0245cd6ff45320987
Lucas Garcia de Araujo Martins <lukasgarcya@hotmail.com>
2019-12-28 11:58:11 -0300

    class Firebase for platform not supported

---
+++ lib/src/unsupported.dart
@@ -0,0 +1,21 @@
+class Firestore {
+  String collection;
+
+  Firestore(this.collection) ;
+
+  Future<String> add(Map<String, dynamic> data) {
+    throw 'Platform Not Supported!';
+  }
+
+  Future<void> update(String documentId, Map<String, dynamic> data) {
+    throw 'Platform Not Supported!';
+  }
+
+  Future<void> delete(String documentId) {
+    throw 'Platform Not Supported!';
+  }
+
+  Stream<dynamic> list() {
+    throw 'Platform Not Supported!';
+  }
+}
