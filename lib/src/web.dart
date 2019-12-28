b4641873c0b62283b7e0f25055d1c082351a2a1a
Lucas Garcia de Araujo Martins <lukasgarcya@hotmail.com>
2019-12-28 14:02:44 -0300

    Firestore create, update, delete and list  in the web

---
+++ lib/src/web.dart
@@ -0,0 +1,47 @@
+import 'dart:async';
+
+import 'package:firebase/firestore.dart' as fs;
+import 'package:firebase/firebase.dart';
+
+class Firestore {
+  String collection;
+  fs.CollectionReference _collection;
+  
+  Firestore(this.collection) {
+    _collection = firestore().collection(this.collection);
+  }
+
+  Future<String> add(Map<String, dynamic> data) async {
+    var add = await _collection.add(data);
+    return add.id;
+  }
+
+  Future<void> update(String documentId, Map<String, dynamic> data) {
+    return _collection.doc(documentId).update(data: data);
+  }
+
+  Future<void> delete(String documentId) {
+    return _collection.doc(documentId).delete();
+  }
+
+  Stream<dynamic> list() {
+    StreamController<Documents> controller = StreamController<Documents>();
+    _collection.onSnapshot.listen((snapshot) {
+      controller.add(Documents(snapshot.docs
+          .map<Document>((document) => Document(document.id, document.data()))
+          .toList()));
+    });
+    return controller.stream;
+  }
+}
+
+class Documents {
+  List<Document> documents;
+  Documents(this.documents);
+}
+
+class Document {
+  String documentID;
+  Map<String, dynamic> data;
+  Document(this.documentID, this.data);
+}
