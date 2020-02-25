5d539afe6b8e66ab10d6d845ca8a0eb1df7e2731
Lucas Garcia de Araujo Martins <lukasgarcya@hotmail.com>
2020-02-25 12:21:42 -0300

    Use methods for generations parts

--- lib/src/repository_generator.dart
+++ lib/src/repository_generator.dart
@@ -1,42 +1,77 @@
-import 'package:analyzer/dart/element/element.dart';
-import 'package:build/src/builder/build_step.dart';
 import 'package:code_builder/code_builder.dart';
 import 'package:flutter_persistence_api/flutter_persistence_api.dart';
 import 'package:crud_generator/crud_generator.dart';
-import 'package:source_gen/source_gen.dart';
 
 class RepositoryGenerator extends GenerateEntityClassForAnnotation<Entity> {
   @override
-  generateForAnnotatedElement(
-      Element element, ConstantReader annotation, BuildStep buildStep) {
-    init();
-    this.element = element;
-    name = '${element.name}Repository';
-    addImportPackage('${element.name.toLowerCase()}.entity.dart');
-    _declareField();
+  String generateName() => '${element.name}${manyToManyPosFix}Repository';
+
+  @override
+  void optionalClassInfo() {
+    if (!manyToMany)
+      addImportPackage('${element.name.toLowerCase()}.entity.dart');
+  }
+
+  generateConstructors() {
+    if (manyToMany)
+      declareConstructor(requiredParameters: [
+        Parameter((b) => b
+          ..name = 'collectionParent'
+          ..type = refer('String')),
+        Parameter((b) => b
+          ..name = 'documentId'
+          ..type = refer('String'))
+      ], body: Code('''firestore = Firestore.documentId(
+                    collectionParent, documentId, '${element.name.toLowerCase()}');
+                    '''));
+  }
+
+  void generateFields() {
+    addImportPackage('package:flutter_persistence_firestore/firestore.dart');
+    if (manyToMany) {
+      declareField(refer('Firestore'), 'firestore');
+    } else {
+      declareField(refer('Firestore'), 'firestore',
+          assignment: Code("Firestore('${element.name.toLowerCase()}')"));
+    }
+
+    elementAsClass.fields.forEach((field) {
+      if (isManyToOneField(field)) {
+        addImportPackage('../${field.type.name.toLowerCase()}/${field.type.name.toLowerCase()}.repository.dart');
+        declareField(
+            refer('${field.type.name}Repository'), '${field.name}Repository');
+      } else if (isManyToManyField(field)) {
+        var type = getGenericTypes(field.type).first.element.name;
+        declareField(
+            refer('${type}ManyToManyRepository'), '${field.name}Repository');
+      }
+    });
+  }
+
+  void generateMethods() {
     _methodAdd();
     _methodUpdate();
     _methodDelete();
     _methodList();
     _methodListManyToOne();
-    return build();
-  }
-
-  void _declareField() {
-    addImportPackage('package:flutter_persistence_firestore/firestore.dart');
-    declareField(refer('Firestore'), 'firestore',
-        assignment: Code("Firestore('${element.name.toLowerCase()}')"));
-    elementAsClass.fields.forEach((field) {
-      if (isManyToOneField(field)) {
-        addImportPackage(
-            '../${field.type.name.toLowerCase()}/${field.type.name.toLowerCase()}.repository.dart');
-        declareField(
-            refer('${field.type.name}Repository'), '${field.name}Repository');
-      }
-    });
   }
 
   void _methodAdd() {
+    var addCode = StringBuffer(
+        'var result = await firestore.add($entityInstance.toMap());');
+    elementAsClass.fields.forEach((field) {
+      if (isManyToManyField(field)) {
+        var type = getGenericTypes(field.type).first.element.name;
+        addCode.writeln('''
+            ${field.name}Repository = 
+            ${type}ManyToManyRepository('${element.name.toLowerCase()}', result.documentID);
+            $entityInstance.${field.name}.forEach((${field.name}) {
+            ${field.name}Repository.add(${field.name});
+            });
+            ''');
+      }
+    });
+    addCode.writeln('return result;');
     declareMethod('add',
         returns: refer('Future<dynamic>'),
         requiredParameters: [
@@ -44,8 +79,7 @@ class RepositoryGenerator extends GenerateEntityClassForAnnotation<Entity> {
             ..name = entityInstance
             ..type = refer(entityClass))
         ],
-        lambda: true,
-        body: Code('firestore.add($entityInstance.toMap())'),
+        body: Code(addCode.toString()),
         modifier: MethodModifier.async);
   }
 
@@ -54,14 +88,12 @@ class RepositoryGenerator extends GenerateEntityClassForAnnotation<Entity> {
         returns: refer('Future<void>'),
         requiredParameters: [
           Parameter((b) => b
-            ..name = 'documentId'
-            ..type = refer('String')),
-          Parameter((b) => b
             ..name = entityInstance
             ..type = refer(entityClass))
         ],
         lambda: true,
-        body: Code('firestore.update(documentId, $entityInstance.toMap())'));
+        body: Code(
+            'firestore.update($entityInstance.documentId, $entityInstance.toMap())'));
   }
 
   void _methodDelete() {
@@ -69,19 +101,21 @@ class RepositoryGenerator extends GenerateEntityClassForAnnotation<Entity> {
         returns: refer('Future<void>'),
         requiredParameters: [
           Parameter((b) => b
-            ..name = 'documentId'
-            ..type = refer('String'))
+            ..name = '$entityInstance'
+            ..type = refer('$entityClass'))
         ],
         lambda: true,
-        body: Code('firestore.delete(documentId)'));
+        body: Code('firestore.delete($entityInstance.documentId)'));
   }
 
   void _methodList() {
     declareMethod('list',
         returns: refer('Stream<List<$entityClass>>'),
         lambda: true,
-        body: Code(
-            'firestore.list().map((snapshot) => snapshot.documents.map<$entityClass>((document) => $entityClass.fromMap(document.documentID, document.data)).toList())'));
+        body: Code('''firestore.list().map((snapshot) => 
+            snapshot.documents.map<$entityClass>((document) => 
+            $entityClass.fromMap(document.documentID, document.data)).toList())
+            '''));
   }
 
   void _methodListManyToOne() {
@@ -98,4 +132,9 @@ class RepositoryGenerator extends GenerateEntityClassForAnnotation<Entity> {
       }
     });
   }
+
+  @override
+  GenerateClassForAnnotation instance() => RepositoryGenerator()
+    ..manyToMany = true
+    ..generateImport = false;
 }
